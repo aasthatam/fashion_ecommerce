b9d4c40bdbc3da1b0a9ca6008ae8d3488870b68b
aasthatam <taastha22@tbc.edu.np>
2025-03-26 13:12:52 +0545

    Added more product images

--- my-fashion-app/src/pages/Collection.jsx
+++ my-fashion-app/src/pages/Collection.jsx
@@ -2,18 +2,7 @@ import React, { useState } from "react";
 import { ChevronRight } from "lucide-react";
 import heartIcon from "../assets/heart1.svg";
 import heartIconFilled from "../assets/heart2.svg";
-import Shirt1 from "../assets/shirt1.png";
-import Shirt2 from "../assets/shirt2.png";
-import Shirt3 from "../assets/shirt3.png";
-import Shirt4 from "../assets/shirt4.png";
-import Shirt5 from "../assets/shirt5.png";
-import Shirt6 from "../assets/shirt6.png";
-import Shirt7 from "../assets/shirt7.png";
-import Shirt8 from "../assets/shirt8.png";
-import Shirt9 from "../assets/shirt9.png";
-import Shirt10 from "../assets/shirt10.png";
-import Shirt11 from "../assets/shirt11.png";
-import Shirt12 from "../assets/shirt12.png";
+import { colorOptions, categoryOptions, fabricsOptions, products } from "../assets/assets";
 
 const FilterDropdown = ({ title, options, selectedOptions, onOptionChange }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -69,38 +58,6 @@ const Collection = () => {
     }));
   };
 
-  const colorOptions = [
-    { name: "Black", count: 6 },
-    { name: "Blue", count: 20 },
-    { name: "Brown", count: 10 },
-    { name: "Neutral", count: 23 },
-    { name: "Gray", count: 16 },
-    { name: "Green", count: 12 },
-    { name: "Pink", count: 10 },
-    { name: "Red", count: 7 },
-    { name: "White", count: 24 },
-    { name: "Yellow", count: 29 },
-  ];
-
-  const categoryOptions = [
-    { name: "Top", count: 6 },
-    { name: "Sweaters", count: 20 },
-    { name: "Pants", count: 10 },
-    { name: "Dresses", count: 23 },
-    { name: "Bottoms", count: 6 },
-    { name: "Bodusuits  & Jumpsuits", count: 6 },
-  ]
-
-  const fabricsOptions = [
-    { name: "Cotton", count: 6 },
-    { name: "Silk", count: 23 },
-    { name: "Nylon", count: 10 },
-    { name: "Rayon", count: 12 },
-    { name: "Wood-Silk", count: 20 },
-    { name: "Viscose Jersy", count: 17 },
-    { name: "Hemp", count: 8 },
-  ]
-
   const sortOptions = [
     { name: "Best Selling" },
     { name: "Alphabetically, A-Z" },
@@ -112,20 +69,7 @@ const Collection = () => {
 
   ]
 
-  const products = [
-    { id: 1, name: "Basic Bae Full Size Ribbed Round Neck Long T-Shirt", price: 1500, image: Shirt1 },
-    { id: 2, name: "Round Neck Tiered Tank Top", price: 3000, image: Shirt2 },
-    { id: 3, name: "Lantern Sleeve O-neck Casual Party Tunic Blouse", price: 3000, image: Shirt3 },
-    { id: 4, name: "Full Size Ribbed Round Neck Short Sleeve T-Shirt", price: 1500, image: Shirt4 },
-    { id: 5, name: "Simon Longsleeve Top", price: 2500, image: Shirt5 },
-    { id: 6, name: "Blouse with Notched Neck", price: 3000, image: Shirt6, tag: "Save 20%" },
-    { id: 7, name: "Halter Neck Blouse With Keyhole Back", price: 2500, image: Shirt7 },
-    { id: 8, name: "Urban Solid Color Lapel Long Sleeves Blouse", price: 2700, image: Shirt8, tag: "Save 20%" },
-    { id: 9, name: "Full Size Round Neck Tank Top", price: 3000, image: Shirt9 },
-    { id: 10, name: "Mads Turtleneck Top", price: 3000, image: Shirt10, tag: "Save 20%" },
-    { id: 11, name: "Lace Top with High Neck", price: 2200, image: Shirt11 },
-    { id: 12, name: "Swiss Dot Blouse with Shirred Flounce Sleeve", price: 3000, image: Shirt12 },
-  ];
+ 
   const handleColorChange = (color) => {
     setSelectedColors((prev) =>
       prev.includes(color) ? prev.filter((c) => c !== color) : [...prev, color]
@@ -156,7 +100,10 @@ const Collection = () => {
   });
 
   const sortedProducts = [...filteredProducts].sort((a, b) => {
-    if (sortBy === "Price, Low to High") {
+    if (sortBy === "Best Selling") {
+      // Sort products by bestselling status first (true comes before false)
+      return b.bestselling ? 1 : -1; // True products come first
+    } else if (sortBy === "Price, Low to High") {
       return a.price - b.price;
     } else if (sortBy === "Price, High to Low") {
       return b.price - a.price;
