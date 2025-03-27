9816ff425556f5e32db722d38ce70b622d43af3d
aasthatam <taastha22@tbc.edu.np>
2025-03-27 23:25:40 +0545

    added some more products in the web page

--- my-fashion-app/src/assets/assets.js
+++ my-fashion-app/src/assets/assets.js
@@ -14,6 +14,108 @@ import Image from "../assets/image.png";
 import Image2 from "../assets/image2.png";
 import Image3 from "../assets/image3.png";
 
+import BabilDress1 from "../assets/BabilDress1.png";
+import BabilDress2 from "../assets/BabilDress2.png";
+import BabilDress3 from "../assets/BabilDress3.png";
+import BabilDress4 from "../assets/BabilDress4.png";
+
+import BelamyDress1 from "../assets/BelamyDress1.png";
+import BelamyDress2 from "../assets/BelamyDress2.png";
+import BelamyDress3 from "../assets/BelamyDress3.png";
+import BelamyDress4 from "../assets/BelamyDress4.png";
+
+import BiddySkirt1 from "../assets/BiddySkirt1.png";
+import BiddySkirt2 from "../assets/BiddySkirt2.png";
+import BiddySkirt3 from "../assets/BiddySkirt3.png";
+import BiddySkirt4 from "../assets/BiddySkirt4.png";
+import BiddySkirt5 from "../assets/BiddySkirt5.png";
+
+import CandelaDress1 from "../assets/CandelaDress1.png";
+import CandelaDress2 from "../assets/CandelaDress2.png";
+import CandelaDress3 from "../assets/CandelaDress3.png";
+import CandelaDress4 from "../assets/CandelaDress4.png";
+
+import ChiffonEventDress1 from "../assets/ChiffonEventDress1.png";
+import ChiffonEventDress2 from "../assets/ChiffonEventDress2.png";
+import ChiffonEventDress3 from "../assets/ChiffonEventDress3.png";
+import ChiffonEventDress4 from "../assets/ChiffonEventDress4.png";
+import ChiffonEventDress5 from "../assets/ChiffonEventDress5.png";
+
+import DolinoTop1 from "../assets/DolinoTop1.png";
+import DolinoTop2 from "../assets/DolinoTop2.png";
+import DolinoTop3 from "../assets/DolinoTop3.png";
+import DolinoTop4 from "../assets/DolinoTop4.png";
+import DolinoTop5 from "../assets/DolinoTop5.png";
+
+import DoviPant1 from "../assets/DoviPant1.png";
+import DoviPant2 from "../assets/DoviPant2.png";
+import DoviPant3 from "../assets/DoviPant3.png";
+import DoviPant4 from "../assets/DoviPant4.png";
+import DoviPant5 from "../assets/DoviPant5.png";
+
+import FelixTop1 from "../assets/FelixTop1.png";
+import FelixTop2 from "../assets/FelixTop2.png";
+import FelixTop3 from "../assets/FelixTop3.png";
+import FelixTop4 from "../assets/FelixTop4.png";
+
+import GretaBodySuit1 from "../assets/GretaBodySuit1.png";
+import GretaBodySuit2 from "../assets/GretaBodySuit2.png";
+import GretaBodySuit3 from "../assets/GretaBodySuit3.png";
+import GretaBodySuit4 from "../assets/GretaBodySuit4.png";
+
+import HighWaistBootCutJean1 from "../assets/HighWaitsBootCutJean1.png";
+import HighWaistBootCutJean2 from "../assets/HighWaitsBootCutJean2.png";
+import HighWaistBootCutJean3 from "../assets/HighWaitsBootCutJean3.png";
+import HighWaistBootCutJean4 from "../assets/HighWaitsBootCutJean4.png";
+
+import ImogeneJumpsuit1 from "../assets/ImogeneJumpsuit1.png";
+import ImogeneJumpsuit2 from "../assets/ImogeneJumpsuit2.png";
+import ImogeneJumpsuit3 from "../assets/ImogeneJumpsuit3.png";
+import ImogeneJumpsuit4 from "../assets/ImogeneJumpsuit4.png";
+
+import LongSleeveKnitTop1 from "../assets/LongSleeveKnitTop1.png";
+import LongSleeveKnitTop2 from "../assets/LongSleeveKnitTop2.png";
+import LongSleeveKnitTop3 from "../assets/LongSleeveKnitTop3.png";
+import LongSleeveKnitTop4 from "../assets/LongSleeveKnitTop4.png";
+import LongSleeveKnitTop5 from "../assets/LongSleeveKnitTop5.png";
+
+import WideLegJean1 from "../assets/RawHemWideLegJeanwithPocket1.png";
+import WideLegJean2 from "../assets/RawHemWideLegJeanwithPocket2.png";
+import WideLegJean3 from "../assets/RawHemWideLegJeanwithPocket3.png";
+import WideLegJean4 from "../assets/RawHemWideLegJeanwithPocket4.png";
+
+import RippedFlareJean1 from "../assets/RippedFlareLegJeansandHighWaist1.png";
+import RippedFlareJean2 from "../assets/RippedFlareLegJeansandHighWaist2.png";
+import RippedFlareJean3 from "../assets/RippedFlareLegJeansandHighWaist3.png";
+import RippedFlareJean4 from "../assets/RippedFlareLegJeansandHighWaist4.png";
+
+import SolidKnittedSweater1 from "../assets/SolidKnitSweater1.png";
+import SolidKnittedSweater2 from "../assets/SolidKnitSweater2.png";
+import SolidKnittedSweater3 from "../assets/SolidKnitSweater3.png";
+import SolidKnittedSweater4 from "../assets/SolidKnitSweater4.png";
+import SolidKnittedSweater5 from "../assets/SolidKnitSweaterRed1.png";
+import SolidKnittedSweater6 from "../assets/SolidKnitSweaterRed2.png";
+import SolidKnittedSweater7 from "../assets/SolidKnitSweaterRed3.png";
+import SolidKnittedSweater8 from "../assets/SolidKnitSweaterRed4.png";
+import SolidKnittedSweater9 from "../assets/SolidKnitSweaterRed5.png";
+
+import TopandPantSet1 from "../assets/SquareNeckTopandWideLegPantSet1.png";
+import TopandPantSet2 from "../assets/SquareNeckTopandWideLegPantSet2.png";
+import TopandPantSet3 from "../assets/SquareNeckTopandWideLegPantSet3.png";
+
+import StraightLegJean1 from "../assets/StraightLegJean1.png";
+import StraightLegJean2 from "../assets/StraightLegJean2.png";
+import StraightLegJean3 from "../assets/StraightLegJean3.png";
+import StraightLegJean4 from "../assets/StraightLegJean4.png";
+import StraightLegJean5 from "../assets/StraightLegJean5.png";
+
+import StraightLegJeanLightWash1 from "../assets/StraightLegJeansLightWashed1.png";
+import StraightLegJeanLightWash2 from "../assets/StraightLegJeansLightWashed2.png";
+import StraightLegJeanLightWash3 from "../assets/StraightLegJeansLightWashed3.png";
+import StraightLegJeanLightWash4 from "../assets/StraightLegJeansLightWashed4.png";
+import StraightLegJeanLightWash5 from "../assets/StraightLegJeansLightWashed5.png";
+import StraightLegJeanLightWash6 from "../assets/StraightLegJeansLightWashed6.png";
+
 export const colorOptions = [
     { name: "Black", count: 6 },
     { name: "Blue", count: 20 },
@@ -66,8 +168,8 @@ export const colorOptions = [
         price: 2500, 
         image: Image, 
         color: "Black", 
-        category: "Top", 
-        fabric: "Cotton",
+        category: "Dresses", 
+        fabric: "Viscose Jersey",
         bestselling: true,
         isNewArrival: true
       },
@@ -76,9 +178,9 @@ export const colorOptions = [
             name: "Halter Neck Ribbed Cropped Top", 
             price: 1500, 
             image: Image2, 
-            color: "Black", 
+            color: "Neutral", 
             category: "Top", 
-            fabric: "Cotton",
+            fabric: "Hemp",
             bestselling: true,
             isNewArrival: true
         },
@@ -87,7 +189,7 @@ export const colorOptions = [
           name: "Long Sleeve Lapel Collar Blazer", 
           price: 3000, 
           image: Image3, 
-          color: "Brown", 
+          color: "Neutral", 
           category: "Top", 
           fabric: "Silk",
           bestselling: true,
@@ -99,20 +201,20 @@ export const colorOptions = [
         name: "Basic Bae Full Size Ribbed Round Neck Long T-Shirt", 
         price: 1500, 
         image: Shirt1, 
-        color: "Black", 
-        category: "Top", 
-        fabric: "Cotton",
+        color: "Brown", 
+        category: "Sweaters", 
+        fabric: "Hemp",
         bestselling: true,
-        isNewArrival: true
+        isNewArrival: false
     },
     { 
         id: 5, 
         name: "Round Neck Tiered Tank Top", 
         price: 3000, 
         image: Shirt2, 
-        color: "Blue", 
-        category: "Dresses", 
-        fabric: "Silk",
+        color: "Red", 
+        category: "Top", 
+        fabric: "Cotton",
         bestselling: true ,
         isNewArrival: true
     },
@@ -121,7 +223,7 @@ export const colorOptions = [
         name: "Lantern Sleeve O-neck Casual Party Tunic Blouse", 
         price: 3000, 
         image: Shirt3, 
-        color: "Brown", 
+        color: "Red", 
         category: "Top",
         fabric: "Nylon",
         bestselling: false,
@@ -132,9 +234,9 @@ export const colorOptions = [
         name: "Full Size Ribbed Round Neck Short Sleeve T-Shirt", 
         price: 1500, 
         image: Shirt4, 
-        color: "White", 
+        color: "Brown", 
         category: "Top", 
-        fabric: "Cotton",
+        fabric: "Hemp",
         bestselling: true,
         isNewArrival: true
     },
@@ -143,9 +245,9 @@ export const colorOptions = [
         name: "Simon Longsleeve Top", 
         price: 2500, 
         image: Shirt5, 
-        color: "Gray", 
-        category: "Sweaters", 
-        fabric: "Rayon",
+        color: "Black", 
+        category: "Top", 
+        fabric: "Silk",
         bestselling: false,
         isNewArrival: true
     },
@@ -154,9 +256,9 @@ export const colorOptions = [
         name: "Blouse with Notched Neck", 
         price: 3000, 
         image: Shirt6, 
-        color: "Pink", 
+        color: "White", 
         category: "Top", 
-        fabric: "Silk", 
+        fabric: "Cotton", 
         tag: "Save 20%",
         bestselling: true,
         isNewArrival: true
@@ -166,8 +268,8 @@ export const colorOptions = [
       name: "Halter Neck Blouse With Keyhole Back", 
       price: 2500, 
       image: Shirt7, 
-      color: "Red", 
-      category: "Dresses", 
+      color: "Black", 
+      category: "Top", 
       fabric: "Viscose Jersey",
       bestselling: false,
       isNewArrival: false
@@ -177,9 +279,9 @@ export const colorOptions = [
       name: "Urban Solid Color Lapel Long Sleeves Blouse",
       price: 2700, 
       image: Shirt8, 
-      color: "Green", 
+      color: "Blue", 
       category: "Shirts", 
-      fabric: "Hemp", 
+      fabric: "Silk", 
       tag: "Save 20%",
       bestselling: true,
       isNewArrival: true
@@ -189,10 +291,10 @@ export const colorOptions = [
       name: "Full Size Round Neck Tank Top", 
       price: 3000, 
       image: Shirt9,
-      color: "Neutral", 
-      category: "Bodysuits & Jumpsuits", 
-      fabric: "Silk",
-      bestselling: false,
+      color: "White", 
+      category: "Top", 
+      fabric: "Hemp",
+      bestselling: true,
       isNewArrival: false
     },
     { 
@@ -200,9 +302,9 @@ export const colorOptions = [
       name: "Mads Turtleneck Top", 
       price: 3000, 
       image: Shirt10, 
-      color: "Yellow", 
+      color: "Neutral", 
       category: "Sweaters", 
-      fabric: "Wood-Silk", 
+      fabric: "Nylon", 
       tag: "Save 20%",
       bestselling: true,
       isNewArrival: true
@@ -212,7 +314,7 @@ export const colorOptions = [
       name: "Lace Top with High Neck",
       price: 2200, 
       image: Shirt11, 
-      color: "Blue", 
+      color: "White", 
       category: "Top", 
       fabric: "Cotton",
       bestselling: false,
@@ -223,10 +325,210 @@ export const colorOptions = [
       name: "Swiss Dot Blouse with Shirred Flounce Sleeve", 
       price: 3000, 
       image: Shirt12, 
-      color: "Brown", 
+      color: "Pink", 
+      category: "Top", 
+      fabric: "Cotton" ,
+      bestselling: true,
+      isNewArrival: true
+    },
+    { 
+      id: 16, 
+      name: "Babil Dress", 
+      price: 4000, 
+      image: BabilDress1, 
+      color: "White", 
+      category: "Dresses", 
+      fabric: "Nylon" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 17, 
+      name: "Belamy Dress", 
+      price: 4000, 
+      image: BelamyDress1, 
+      color: "Black", 
+      category: "Dresses", 
+      fabric: "Nylon" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 18, 
+      name: "Biddy Skirt", 
+      price: 3500, 
+      image: BiddySkirt1, 
+      color: "Black", 
+      category: "Bottoms", 
+      fabric: "Cotton" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 19, 
+      name: "Candela Dress", 
+      price: 3500, 
+      image: CandelaDress1, 
+      color: "White", 
+      category: "Dresses", 
+      fabric: "Cotton" ,
+      bestselling: false,
+      isNewArrival: false
+    },
+    { 
+      id: 20, 
+      name: "Chiffon Event Dress", 
+      price: 3500, 
+      image: ChiffonEventDress1, 
+      color: "Black", 
+      category: "Dresses", 
+      fabric: "Cotton" ,
+      bestselling: false,
+      isNewArrival: false
+    },
+    { 
+      id: 21, 
+      name: "Dolino Top", 
+      price: 2500, 
+      image: DolinoTop1, 
+      color: "Black", 
+      category: "Dresses", 
+      fabric: "Hemp" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 22, 
+      name: "Dovi Pant", 
+      price: 3000, 
+      image: DoviPant1, 
+      color: "Gray", 
       category: "Pants", 
+      fabric: "Hemp" ,
+      bestselling: true,
+      isNewArrival: true
+    },
+    { 
+      id: 23, 
+      name: "Felix Top", 
+      price: 4500, 
+      image: FelixTop1, 
+      color: "Black", 
+      category: "Top", 
+      fabric: "Cotton" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 24, 
+      name: "Greta Body Suit", 
+      price: 4500, 
+      image: GretaBodySuit1, 
+      color: "Black", 
+      category: "Bodysuits & Jumpsuits", 
+      fabric: "Cotton" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 25, 
+      name: "High Waist Boot Cut Jean", 
+      price: 4500, 
+      image: HighWaistBootCutJean1, 
+      color: "Blue", 
+      category: "Pants", 
+      fabric: "Cotton" ,
+      bestselling: true,
+      isNewArrival: true
+    },
+    { 
+      id: 26, 
+      name: "Imogene JumpSuit", 
+      price: 4500, 
+      image: ImogeneJumpsuit1, 
+      color: "Black", 
+      category: "Bodysuits & Jumpsuits", 
       fabric: "Nylon" ,
       bestselling: true,
       isNewArrival: true
     },
+    { 
+      id: 27, 
+      name: "Long Sleeve Knit Top", 
+      price: 4500, 
+      image: LongSleeveKnitTop1, 
+      color: "White", 
+      category: "Sweaters", 
+      fabric: "Hemp" ,
+      bestselling: true,
+      isNewArrival: true
+    },
+    { 
+      id: 28, 
+      name: "Raw Hem Wide Leg Jean with Pocket", 
+      price: 4500, 
+      image: WideLegJean1, 
+      color: "Blue", 
+      category: "Pants", 
+      fabric: "Cotton" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 29, 
+      name: "Ripped Flare Leg Jeans and High Waist", 
+      price: 4500, 
+      image: RippedFlareJean1, 
+      color: "Blue", 
+      category: "Pants", 
+      fabric: "Cotton" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 30, 
+      name: "Solid Knit Sweater", 
+      price: 4500, 
+      image: SolidKnittedSweater1, 
+      color: "Neutral", 
+      category: "Sweaters", 
+      fabric: "Cotton" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 31, 
+      name: "Square Neck Top and Wide Leg Pant Set", 
+      price: 4500, 
+      image: TopandPantSet1, 
+      color: "Black", 
+      category: "Pants", 
+      fabric: "Cotton" ,
+      bestselling: true,
+      isNewArrival: true
+    },
+    { 
+      id: 32, 
+      name: "Straight Leg Jean", 
+      price: 4500, 
+      image: StraightLegJean1, 
+      color: "Black", 
+      category: "Pants", 
+      fabric: "Cotton" ,
+      bestselling: false,
+      isNewArrival: true
+    },
+    { 
+      id: 33, 
+      name: "Straight Leg Jean Light Washed", 
+      price: 4500, 
+      image: StraightLegJeanLightWash1 , 
+      color: "Blue", 
+      category: "Pants", 
+      fabric: "Cotton" ,
+      bestselling: true,
+      isNewArrival: true
+    },
+    
+    
   ];
