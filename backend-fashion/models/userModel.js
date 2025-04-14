c93dbdd42ff1a6ef8a8c560d05d420e57fdca333
aasthatam <taastha22@tbc.edu.np>
2025-04-14 10:11:55 +0545

    changes made in the file

--- backend-fashion/models/userModel.js
+++ backend-fashion/models/userModel.js
@@ -5,13 +5,13 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["customer", "admin"], default: "customer" },
-    cart: [{
-        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-        quantity: { type: Number, required: true, default: 1 },
-        price: { type: Number, required: true }, // You can store the price at the time of adding to cart.
-        addedAt: { type: Date, default: Date.now }
-    }],
-    cartTotal: { type: Number, default: 0 } // Optional, you can update it on each cart change.
+    cart: { type: Object, default: {}
+        // productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        // quantity: { type: Number, required: true, default: 1 },
+        // price: { type: Number, required: true }, // You can store the price at the time of adding to cart.
+        // addedAt: { type: Date, default: Date.now }
+    },
+    // cartTotal: { type: Number, default: 0 } // Optional, you can update it on each cart change.
 }, { minimize: false })
 
 const userModel = mongoose.models.user || mongoose.model('user', userSchema);
