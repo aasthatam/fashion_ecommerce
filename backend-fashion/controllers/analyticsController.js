543455e4268fe39d58680d5a35971e8f208a17a5
aasthatam <taastha22@tbc.edu.np>
2025-05-06 10:58:05 +0545

    removed the commented code in  analytics controller

--- backend-fashion/controllers/analyticsController.js
+++ backend-fashion/controllers/analyticsController.js
@@ -1,62 +1,6 @@
 import userBehaviorModel from "../models/userBehaviorModel.js";
 import mongoose from "mongoose";
 
-// const getBehaviorStats = async (req, res) => {
-//   try {
-//     const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
-
-//     const stats = await userBehaviorModel.aggregate([
-//       {
-//         $match: {
-//           timestamp: { $gte: sevenDaysAgo }
-//         }
-//       },
-//       {
-//         $group: {
-//           _id: {
-//             productId: "$productId",
-//             date: { $dateToString: { format: "%Y-%m-%d", date: "$timestamp" } },
-//             action: "$action"
-//           },
-//           count: { $sum: 1 }
-//         }
-//       },
-//       {
-//         $lookup: {
-//           from: "products",
-//           let: { pid: "$_id.productId" },
-//           pipeline: [
-//             { $match: { $expr: { $eq: ["$_id", "$$pid"] } } }
-//           ],
-//           as: "productInfo"
-//         }
-//       },
-//       {
-//         $unwind: {
-//           path: "$productInfo",
-//           preserveNullAndEmptyArrays: true
-//         }
-//       },
-//       {
-//         $project: {
-//           date: "$_id.date",
-//           action: "$_id.action",
-//           productId: "$_id.productId",
-//           productName: "$productInfo.name",
-//           count: 1
-//         }
-//       },
-//       {
-//         $sort: { date: 1 }
-//       }
-//     ]);
-    
-
-//     res.json({ success: true, stats });
-//   } catch (error) {
-//     res.json({ success: false, message: error.message });
-//   }
-// };
 const getBehaviorStats = async (req, res) => {
   try {
     const { userId } = req.query;
