be0010d96bfbe989d7597d09ec3fd31782710357
aasthatam <taastha22@tbc.edu.np>
2025-05-07 16:33:40 +0545

    added cancel route

--- backend-fashion/routes/orderRoute.js
+++ backend-fashion/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { placeOrder, allOrders, userOrders, updateStatus } from "../controllers/orderController.js"
+import { placeOrder, allOrders, userOrders, updateStatus, cancelOrder } from "../controllers/orderController.js"
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 import { createPayment, executePayment } from "../controllers/paypalController.js"
@@ -18,5 +18,7 @@ orderRouter.post('/userorders', authUser, userOrders )
 
 orderRouter.post('/paypal/create', authUser, createPayment);
 orderRouter.post('/paypal/execute', authUser, executePayment);
+
+orderRouter.post('/cancel', authUser, cancelOrder);
 
 export default orderRouter
