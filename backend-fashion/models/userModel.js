f16a6ea728f35d716bd65c01fff1c94a09b8861b
aasthatam <taastha22@tbc.edu.np>
2025-05-07 20:39:46 +0545

    added notification table

--- backend-fashion/models/userModel.js
+++ backend-fashion/models/userModel.js
@@ -18,6 +18,14 @@ const userSchema = new mongoose.Schema({
         type: [String],
         default: []
       },
+    notifications: [
+        {
+          productId: { type: String },
+          message: { type: String },
+          createdAt: { type: Date, default: Date.now },
+          read: { type: Boolean, default: false }
+        }
+    ]  
 }, { minimize: false })
 
 const userModel = mongoose.models.user || mongoose.model('user', userSchema);
